7357096f2f66878b61fce2d590c0a434511c3028
4l155t0r <4l155t0r@proton.me>
2026-01-17 17:00:39 +0200

    6_3 added shots

--- shot.py
+++ shot.py
@@ -1,10 +1,10 @@
 import pygame
+from constants import SHOT_RADIUS
 from circleshape import CircleShape
 
-class Asteroid(CircleShape):
-    
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), (int(self.position.x), int(self.position.y)), self.radius)
